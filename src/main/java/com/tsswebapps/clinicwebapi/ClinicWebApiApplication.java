177bfc6c0b31ba4d24df65f2da72d95f01a3de1a
thiago-santanna <uzmaki_thiago@outlook.com>
2023-02-14 05:32:01 -0300

    Start Project

---
+++ src/main/java/com/tsswebapps/clinicwebapi/ClinicWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.clinicwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsswebapps/clinicwebapi/ClinicWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.clinicwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
